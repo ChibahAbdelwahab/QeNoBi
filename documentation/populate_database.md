b7ca78c74df222dbc02bdb8e768f82e50d42a84c
Abdelwahab <abdelwahab.chibah@dataimpact.io>
2020-10-14 17:51:12 +0200

    database population script with documentation

--- documentation/populate_database.md
+++ documentation/populate_database.md
@@ -2,8 +2,8 @@
 
 Use the database_population.ipynb in utils to load random data in the database,
 Some parameters are possible to control the data : 
-* `nb_transactions` = 1000
-* `customers_range` = (0, 1000)
+* `nb_transactions` = 100000
+* `customers_range` = (0, 50)
 * `articles_range` = (0, 1000)
 * `stations_range` = (0, 1000)
 * `date_min` = datetime(2017, 1, 1)
--- tests/sankey_generator_test.py
+++ tests/sankey_generator_test.py
@@ -8,7 +8,7 @@ from settings.settings import GROUPS_FOLDER
 
 def test_sankey_generator():
     sg = SankeyGenerator()
-    input_file = "M-10-[1-2]-[sex]-lcm.out"
+    input_file = "M-30-[2-10]-[sex]-lcm.out"
     e = sg.sankey_preprocessing(input_file, keep_all_groups_in_periods=[])
     assert os.path.isfile(f"{GROUPS_FOLDER}/{input_file}")
     assert os.path.isfile(sg.generate_sankey_file(input_file))
