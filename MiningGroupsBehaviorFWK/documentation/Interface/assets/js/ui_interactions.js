665fcaae05d18f5835732e6cfc05f42c3b425303
Abdelwahab <abdelwahab.chibah@dataimpact.io>
2020-11-08 10:10:22 +0100

    Addded interface design

---
+++ MiningGroupsBehaviorFWK/documentation/Interface/assets/js/ui_interactions.js
@@ -0,0 +1,22 @@
+$('button').on('click', function () {
+        $('button').removeClass('active');
+        $(this).addClass('active');
+    }
+);
+console.log("HELLLO")
+$("#customer-centric-selection").show();
+$("#product-centric-selection").hide()
+
+$("#product-centric-tab").on("click", function () {
+    $("#customer-centric-selection").hide();
+    $("#product-centric-selection").show()
+
+})
+
+
+$("#customer-centric-tab").on("click", function () {
+    $("#customer-centric-selection").show();
+    $("#product-centric-selection").hide()
+
+})
+$('select').selectpicker();
